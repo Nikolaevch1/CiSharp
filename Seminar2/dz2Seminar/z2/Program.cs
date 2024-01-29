cd6529d37d402e5bcda112cd9f89c835a2f002a7
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-29 15:57:26 +0500

    Подготовка к сдаче дз

---
+++ Seminar2/dz2Seminar/z2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+
+Console.Write("Введите значение X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x != 0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("I четверть");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("II четверть");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("III четверть");
+    }
+    else
+    {
+        Console.WriteLine("IV четверть");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка: Координаты не могут быть равны нулю.");
+}
---
+++ Seminar2/dz2Seminar/z3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+//  и показывает наибольшую цифру числа.
+
+int num;
+int maxDigit = 0;
+
+Console.Write("Введите значение от 10 до 99: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10 || num > 99)
+{
+    Console.WriteLine("Число вне указанного отрезка");
+    return;
+}
+
+while (num != 0)
+{
+
+    int currentDigit = num % 10;
+    num = num / 10;
+
+    if (currentDigit > maxDigit)
+    {
+        maxDigit = currentDigit;
+    }
+}
+
+Console.WriteLine($"Наибольшая цифра числа: {maxDigit}");
