424f871533669cd45f43e719d29edc7638d985a2
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-29 15:56:18 +0500

    Закончен семинар и выполнено домашнее задание

---
+++ Seminar2/dz2Seminar/z4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N,
+//  а на выходе показывает его цифры через запятую.
+
+
+Console.WriteLine("Введите любое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = number.ToString();
+foreach (char c in result)
+{
+    Console.Write(c);
+    Console.Write(", ");
+}
+Console.ReadKey();
