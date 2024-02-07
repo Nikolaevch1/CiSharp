e82cd2a9e1175e8a8ffc9e15832251826e3719aa
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-07 16:31:18 +0500

    Первая задача Семинара

---
+++ funcion/SeminarF/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int MyFunction(int a, int b, int c)
+{
+    int res = a * b * c;
+    return res;
+}
+
+int Add(int a, int b, int c)
+{
+    int res = a + b + c;
+    return res;
+}
+int res = MyFunction(5, 6, 7);
+
+System.Console.WriteLine(MyFunction(5, 6, 7));
+System.Console.WriteLine(res);
+
+System.Console.WriteLine(Add(1, 2, 3));
