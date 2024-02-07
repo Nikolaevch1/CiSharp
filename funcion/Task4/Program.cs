b6dbec29427b83ce72a17d51344659cc0409c355
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-07 23:25:13 +0500

    add dirikt Task4

--- funcion/Task4/Program.cs
+++ funcion/Task4/Program.cs
@@ -6,15 +6,15 @@
 // while(i < size)
 // {
 //     arr_int[i] = rnd.Next(1, 10);
-//     i = i + 1;
-// }
+// //     i = i + 1;
+// // }
 
-// i = 0;
-// while(i < size)
-// {
-//     System.Console.Write($"{arr_int[i]} ");
-//     i = i + 1;
-// }
+// // i = 0;
+// // while(i < size)
+// // {
+// //     System.Console.Write($"{arr_int[i]} ");
+// //     i = i + 1;
+// // }
 
 //                      Заполнеие с клавиатуры
 
@@ -35,3 +35,29 @@ while(i < size)
     System.Console.Write($"{arr_int[i]} ");
     i = i + 1;
 }
+
+int GetArrayFromConsole() // Функция ручнного ввода размера и элементов массива
+{
+    Console.Write("add Size: ");
+    int size = int.Parse(Console.ReadLine()!);
+    int[] array = new int[size];
+
+    Console.WriteLine($"Enter {size} element:  ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // Функция вывода 
+{
+    Console.Write("[");
+    foreach (int number in array)
+    {
+        Console.Write(number + " ");
+    }
+    Console.WriteLine("]");
+}
+
+
