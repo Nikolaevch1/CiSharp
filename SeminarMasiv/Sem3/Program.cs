4864477c4568cd667772b0a622eb40e81e10e221
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-08 23:28:19 +0500

    Решение 3 задачи

--- SeminarMasiv/Sem3/Program.cs
+++ SeminarMasiv/Sem3/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Создание массива
+int[,] GetArray(int rows, int cols, int minValue = 0, int maxValue = 10)
+{
+    int[,] table = new int[rows, cols];
+
+    var rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+
+    return table;
+}
+
+// Вывод массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] MeansRows(int[,] matrix) // Вывод дробной
+{
+    double[] means = new double[matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            means[i] += matrix[i, j];
+        }
+        means[i] /= matrix.GetLength(1);
+    }
+
+    return means;
+}
+
+int[,] matrix = GetArray(3, 2);
+PrintArray(matrix);
+
+double[] means = MeansRows(matrix);
+foreach (double item in means)
+{
+    Console.WriteLine(item);
+}
