24c4031823bfb4c18f765a43b82edb444b0a894c
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-08 15:21:30 +0500

    4 урок Фукция break и continue

---
+++ LesMas2/ma2iv3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromStr(string s) // Функция считывающая от пользователя и извлекающая толко строки
+{
+    string letters = "";
+    foreach (char element in s)
+    {
+        if(char.IsAsciiLetter(element) == true)
+        {
+            letters = letters + element;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine()!;
+string letters = GetLettersFromStr(str);
+System.Console.WriteLine(letters);
