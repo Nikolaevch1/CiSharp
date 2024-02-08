8a6582a4bf14767ceed9cef0d8706498d088eb28
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-08 14:20:00 +0500

    4 урок Фукция создания двухмерного массива и функция вывода

--- LesMas2/ma2iv1/Program.cs
+++ LesMas2/ma2iv1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] GreateMatrix(
+    int rowsCount, int columnsCount) // Функция создания двумерного массива
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix) // Функция ввывода матицы
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // интерплояция строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = GreateMatrix(4, 5);
+
+ShowMatrix(matrix);
