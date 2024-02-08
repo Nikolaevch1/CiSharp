127dae768b930f690fe0d01d4f24a16f6e893652
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-08 18:08:53 +0500

    Сделано дз по функциям "Задано"

--- LesFunction/Task1/Program.cs
+++ LesFunction/Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+using System;
+
+class Program {
+static void Main() {
+// Инициализация массива
+int[] arr = GetRandomArray(10);
+
+    // Вызов функции для подсчета четных чисел
+    Console.WriteLine($"Количество четных чисел: {CountEvenNumbers(arr)}");
+}
+
+// Функция для генерации случайного массива чисел
+static int[] GetRandomArray(int size) {
+    Random random = new Random();
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        result[i] = random.Next(100);
+    }
+
+    return result;
+}
+
+// Функция для подсчета четных чисел в массиве
+static int CountEvenNumbers(int[] arr) {
+    int count = 0;
+    foreach (var number in arr) {
+        if (number % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+}
---
+++ LesFunction/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rndGen = new Random();
+            int[] inputArray = Enumerable.Range(1, 10).Select(i => rndGen.Next(0, 20)).ToArray();
+            
+            ReverseArray reverseArray = new ReverseArray();
+            inputArray = reverseArray.Reverse(inputArray);
+            
+
+            foreach (int i in inputArray)
+            {
+                Console.Write($"{i} "!);
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+
+    internal class ReverseArray
+    {
+        public int[] Reverse(int[] input)
+        {
+            List<int> list = new List<int>(input);
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
