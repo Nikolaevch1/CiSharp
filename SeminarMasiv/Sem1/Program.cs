d85df3860f3717a810b16b38f9ba61414ad8d0b0
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-08 23:29:40 +0500

    Решение 1 задачи

--- SeminarMasiv/Sem1/Program.cs
+++ SeminarMasiv/Sem1/Program.cs
@@ -2,43 +2,45 @@
 // Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
 // и замените эти элементы на их квадраты.
 
-// Создание рандомного двухмерного массива
-int[,] CreateRndMatrix(int rowsCount, int columsCount)
+int[,] CreateRndMatrix(int rowsCount, int columnsCount) // Создание рандомного двухмерного массива
 {
-    int[,] matrix = new int[rowsCount, columsCount];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rnd.Next(1, 10);
-        }
-    }
+int[,] matrix = new int[rowsCount, columnsCount];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(1, 10);
+}
+}
+return matrix;
 }
 
-// Функция печати матрицы
+//Функция Печать матрицы
 void ShowMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength; j++)
-        {
-            System.Console.Write($"{matrix[i, j]}\t");
-        }
-        System.Console.WriteLine();
-    }
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]}\t");
+}
+System.Console.WriteLine();
+}
 }
 
 int[,] matrix = CreateRndMatrix(4, 4);
+ShowMatrix(matrix);
+System.Console.WriteLine();
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int j = 0; j < matrix.GetLength; j++)
-    {
-        if (i % 2 == 0 && j % 2 == 0)
-        {
-            matrix[i, j] = matrix[i, j] * matrix[i, j];
-        }
-    }
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+if (i % 2 == 0 && j % 2 == 0 && i == j)
+{
+matrix[i, j] = matrix[i, j] * matrix[i, j];
 }
-ShowMatrix(matrix);
+}
+}
 
+ShowMatrix(matrix);
--- SeminarMasiv/Sem2/Program.cs
+++ SeminarMasiv/Sem2/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
+//  (с индексами (0,0); (1;1) и т.д.
+
+int[,] CreateRndMatrix(int rowsCount, int columnsCount) // Создание рандомного двухмерного массива
+{
+int[,] matrix = new int[rowsCount, columnsCount];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = rnd.Next(1, 10);
+}
+}
+return matrix;
+}
+
+//Функция Печать матрицы
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int[,] matrix = CreateRndMatrix(4, 4);
+
+// Функция ссумирования диагонали
+int SumDiag(int[,] matrix)
+{
+    int sum = 0;
+    int minLength = Math.Min(matrix.GetLength(0),matrix.GetLength(1));
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum = sum + matrix[i, i];
+    }
+    return sum;
+}
+
+ShowMatrix(matrix);
+int sum = SumDiag(matrix);
+System.Console.WriteLine($"Сумма = {sum}");
