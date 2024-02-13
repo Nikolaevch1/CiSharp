166dd82d1fd983eb447419caf159108cf8ea6dee
Nikolaev1ch <vsharanov@yandex.ru>
2024-02-13 15:07:13 +0500

    Записан 6 Семинар

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задание 2. Работа в сессионных залах
+// Семинар 6. Массивы и строки
+// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
+
+string str = "Hello!";
+
+char[] MakeCharsArray(string str)
+{   
+    int size = str.Length;
+    char[] word = new char[size];
+    for (int i = 0; i < str.Length; i++)
+    {
+        word[i] = str[i];
+    }
+    return word;
+}
+char[] word = MakeCharsArray(str);
+foreach (char e in word)
+{
+    System.Console.Write($"{e} ");
+}
---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+bool IsVowes(char chr) // функция выбирает гласные 
+{
+    return  chr == 'a' || chr == 'e' ||
+            chr == 'i' || chr == 'o' ||
+            chr == 'u' || chr == 'y';
+}
+int CountVolwels(string str) // Функция считает количестов гласных
+{
+    int vowels = 0;
+    foreach (char e in str)
+    {
+        if (IsVowes(e))
+        {
+            vowels++;
+        }
+    }
+    return vowels;
+}
+System.Console.WriteLine("Введите произвольную строку: ");
+string str2 = Console.ReadLine()!;
+int res = CountVolwels(str2);
+System.Console.WriteLine($"Количество гласных: {res}");
